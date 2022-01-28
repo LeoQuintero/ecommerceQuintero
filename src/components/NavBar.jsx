976ab382172a78c8f7b36062e60d0e986d829003
LeoQuintero <leoquintero7@gmail.com>
2022-01-27 22:56:40 -0500

    creamos estructura Navbar + Quintero

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function NavBar() {
+    return (
+    <div>
+        <ul>
+            <div>
+                <li>MiLogo</li>
+            </div>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Celulares</li>
+            <li>Laptos</li>
+        </ul>
+    </div>
+    );
+}
+
