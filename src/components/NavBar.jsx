145d8322d2cfb3702b7ad207fcb1a7158daad835
LeoQuintero <leoquintero7@gmail.com>
2022-01-28 22:55:21 -0500

    NavBar completo + Quintero

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,25 @@
 import React from "react";
+import myLogo from "../img/logoCoder.png";
 
 export default function NavBar() {
     return (
-    <div>
-        <ul>
-            <div>
-                <li>MiLogo</li>
-            </div>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Celulares</li>
-            <li>Laptos</li>
-        </ul>
+<div> 
+    <nav className="navbar navbar-light bg-light">
+    <div className="container">
+        <a className="navbar-brand" href="#">
+        <img src={myLogo} alt="coderHouse" width="50"/>
+        </a>
+        <a className="btn btn-primary" href="#" role="button">About</a>
+        <button className="btn btn-primary" type="submit">Contact </button>   
+        <input className="btn btn-primary" type="button" value="Cell phones"/>
+        <input className="btn btn-primary" type="submit" value="Laptops"/>
+        <input className="btn btn-primary" type="reset" value="Tablets"/>
     </div>
+    </nav>   
+    
+    
+ </div>
+
     );
 }
 
